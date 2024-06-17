cd9e1fff48df609f886db8fb4a4948d390270e1a
MichaelSheveland <shevelands@yahoo.com>
2024-06-17 16:48:34 -0500

    last add

--- M3_Starter_Code/savings_account.py
+++ M3_Starter_Code/savings_account.py
@@ -1,6 +1,6 @@
 """Import the Account class from the Account.py file."""
 # ADD YOUR CODE HERE
-from savings_account import Account
+from Account import Account
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
     """Creates a savings account, calculates interest earned, and updates the account balance.
@@ -17,7 +17,7 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    savings_account = Account(balance)
+    savings_account = Account(balance, 0.0)
     # Calculate interest earned
      # ADD YOUR CODE HERE
     interest_earned = balance * (interest_rate / 100) * (months / 12)
